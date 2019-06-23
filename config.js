a9ef76d17ff24fbbecff6b0af6c7373ff128017b
MichalAW <michal.wantaeuco@gmail.com>
2019-06-23 17:54:51 +0200

    pug

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '770818792922-qpbnpopta2bo5enkdqr9fuu8nsjghc9k.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'gq-yEhDIczQyH6mddZ1yEcZH',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
--- server.js
+++ server.js
@@ -1,25 +1,56 @@
 var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
 var app = express();
+var googleProfile = {};
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+},
+    function (accessToken, refreshToken, profile, cb) {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName
+        };
+        cb(null, profile);
+    }
+));
 
 app.set('view engine', 'pug');
 app.set('views', './views');
-app.use('/store', function (req, res, next) {
-    console.log('I am an intermediary when requesting / store');
-    next();
-});
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/login', function (req, res) {
-    res.render('login', {
-        name: "login to google",
-        url: "http://www.google.com"
+app.get('/', function (req, res) {
+    res.render('index', {
+        user: req.user
     });
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello world!');
+app.get('/logged', function (req, res) {
+    res.render('logged', {
+        user: googleProfile
+    });
 });
 
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/logged',
+        failureRedirect: '/'
+    }));
+
 app.listen(3000);
-app.use(function (req, res, next) {
-    res.status(404).send('Sorry, we could not do what you want!')
-});
